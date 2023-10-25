d6732d086d339c53a7d21de4daf5a44b43374bcf
Grady Wright <gradywright25@amdg.rockhursths.edu>
2023-10-25 11:55:53 -0500

    save 2

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 public class Main{
     public static void main(String[] args){
         try {
-            IntVector test = new IntVector(-1000);
+            IntVector test = new IntVector();
 
             System.out.println("Size: " + test.size());
 
+            test.add(5);
+            test.add(2, 1);
+            System.out.println(test);
             for(int i = 0; i < test.size(); i++){}
-
+               
 
         } catch(IllegalArgumentException ex){
             System.out.println("That's Illegal!");
@@ -15,5 +18,6 @@ public class Main{
         }catch(Exception ex){
             System.out.println("Generic Exception");
         }
+       
     }    
 }
