55afac75e2d059149ace26849915f70affe8ecaf
Grady Wright <gradywright25@amdg.rockhursths.edu>
2023-10-31 11:50:04 -0500

    final draft

--- IntVector.java
+++ IntVector.java
@@ -15,4 +15,96 @@ public class IntVector {
     }
 
     public int size(){return size;}
+
+    //CRUD is 99% of business
+    // CRUD, Create, Read, Update, Delete
+
+    private void testBounds(int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    
+    private void tryResize(){
+        if(size >= arr.length-1){
+            //making new temp array
+            int[] temp = new int[1 + arr.length * 2];
+            //copy data over
+            for(int i = 0; i < size; i++){
+                temp[i] = arr[i];
+            }
+            //set old array to temp array
+            arr = temp;
+        }
+    }
+    //insert the value n at the end of the vector
+    public void add(int n){
+        tryResize(); //gaurntee capacity first
+        arr[size++] = n;
+    }
+
+    // Read
+    public int get(int index){
+        testBounds(index);
+        return arr[index];
+    }
+    // Clear
+    public void clear(){
+        arr = new int[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public void set(int index, int value){
+        testBounds(index);
+        arr[index] = value;
+    }
+/**
+ * Sets the elemetn at 'index' eqaul to 'value'
+ * Shifts elements to the right
+ * Returns 'true' if the value was successfully inserted, false otherwis
+ * @param index The position i which the value will be inserted
+ * @param value The numerical value to be added
+ */
+
+
+public boolean add(int index, int value){
+   testBounds(index);
+   tryResize();
+   for(int i = size + 1; i > index; i--){
+    arr[i] = arr[i-1];
+   }
+   arr[index] = value;
+   size++;
+   return true;
+}
+
+/**
+ * Removes the first instance of 'value' found in the list
+ * Shifts remaining values if neccessary
+ * Returns 'true' if the value was successfully removed, false otherwis
+ *The list remains unchanged if the value does not exist in the list
+ * @param value The numerical value to be removed if it exists
+ */
+
+ public boolean remove(int value){
+   boolean found = false;
+   for(int i = 0; i < size-1; i++){
+    if(arr[i] == value || found){
+       found = true;
+       arr[i] = arr[i+1];
+    }
+   }
+   if(found) size--;
+   return found;
+ }
+ 
+    @Override
+    public String toString(){
+        if(size == 0) return"[]"; //empty array
+        String t = "[" + arr[0];
+        for(int i = 1; i < size; i++){
+            t += ", " + arr[i];
+        }
+        return t + "]";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
 public class Main{
     public static void main(String[] args){
         try {
-            IntVector test = new IntVector(-1000);
+            IntVector test = new IntVector();
 
             System.out.println("Size: " + test.size());
 
+            for(int i = 0; i < 20; i++){
+                test.add(1);
+            }
+            test.add(7, 5);
+            test.add(2, 4);
+            System.out.println(test);
+            test.remove(4);
+            System.out.println(test);
             for(int i = 0; i < test.size(); i++){}
-
+               
 
         } catch(IllegalArgumentException ex){
             System.out.println("That's Illegal!");
@@ -15,5 +23,6 @@ public class Main{
         }catch(Exception ex){
             System.out.println("Generic Exception");
         }
+       
     }    
 }
